c21cd9320d2ef9a5237366bc7d218ca1db1d33f6
IlanVinograd <ilanv1111@gmail.com>
2025-04-10 21:54:58 +0300

    refactored cpu files

--- src/Sources/cpu.cpp
+++ src/Sources/cpu.cpp
@@ -1,24 +1,26 @@
 #include "../Includes/cpu.hpp"
 
 void CPU::setReg(u8 reg, u8 bitmap) {
-    if(reg > 16) throw std::out_of_range("Out Of Range");
+    if(reg >= 16) throw std::out_of_range("Out Of Range");
     V[reg] = bitmap;
 }
 
 u8 CPU::getReg(u8 reg) const {
+    if(reg >= 16) throw std::out_of_range("Out Of Range");
     return V[reg];
 }
 
 void CPU::push(u8 byte) {
-    SP++;
-    stack[SP] =  byte;
+    if (SP >= 64) throw std::overflow_error("Stack overflow");
+    stack[SP++] = byte;
 }
 
 void CPU::pop() {
-    stack[SP] = 0;
-    SP--;
+    if (SP == 0) throw std::underflow_error("Stack underflow");
+    stack[--SP] = 0;
 }
 
 u8 CPU::peek() {
-    return stack[SP];
+    if (SP == 0) throw std::underflow_error("Stack underflow");
+    return stack[SP - 1];
 }
